f240b06e11be81f6ba6137b16ee46f189ee1c1ab
Przemysław Szafraniec <39964594+Nimplex@users.noreply.github.com>
2025-09-24 00:33:47 +0200

    Removed unnecessary self-closing slashes from tags

--- public/register.php
+++ public/register.php
@@ -2,8 +2,8 @@
 <html lang="pl">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Rejestracja</title>
 </head>
 
@@ -12,7 +12,7 @@
         <label>Nazwa użytkownika: <input type="text" name="username" required></label><br>
         <label>E-mail: <input type="email" name="email" required></label><br>
         <label>Hasło: <input type="password" name="password" required></label><br>
-        <input type="submit">Zarejestruj się</input>
+        <input type="submit" value="Zarejestruj się">
     </form>
 </body>
 
