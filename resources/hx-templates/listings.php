968cf869812106c3f207cfa04fb6a164038dca7e
feiMopps <132297930+feiMopps@users.noreply.github.com>
2025-11-20 18:42:54 +0100

    fix fade-in for incoming elements

--- resources/hx-templates/listings.php
+++ resources/hx-templates/listings.php
@@ -7,49 +7,47 @@ $is_raw = $_SERVER['HTTP_RAW_REQUEST'] ?? null;
 ?>
 
 <?php foreach ($listing->listAll($page, $_SESSION['user_id']) as $lis): ?>
-<div class="offer-card<?= $is_raw ? ' settling' : '' ?>">
-    <article>
-        <a href="/listings/view.php?listing=<?= urlencode($lis['listing_id']) ?>" role="link">
-            <?php if (!empty($lis['cover_file_id'])):
-                $encoded_file = urlencode($lis['cover_file_id']); ?>
-            <header>
-                <img height="300" src="/covers.php?file=<?= $encoded_file ?>" alt="<?= htmlspecialchars($lis['title']) ?>">
-            </header>
-            <?php endif; ?>
+<article class="<?= $is_raw ? 'settling' : '' ?>">
+    <a href="/listings/view.php?listing=<?= urlencode($lis['listing_id']) ?>" role="link">
+        <?php if (!empty($lis['cover_file_id'])):
+            $encoded_file = urlencode($lis['cover_file_id']); ?>
+        <header>
+            <img height="300" src="/covers.php?file=<?= $encoded_file ?>" alt="<?= htmlspecialchars($lis['title']) ?>">
+        </header>
+        <?php endif; ?>
 
-            <main>
-                <h2><?= htmlspecialchars($lis['title']) ?></h2>
-                <ul>
-                    <li><p><?= htmlspecialchars($lis['display_name']) ?></p></li>
-                    <li><p><?= htmlspecialchars($lis['created_at']) ?></p></li>
-                </ul>
-                <span class="small-text">Naciśnij aby wyświetlić więcej szczegółów</span>
-            </main>
-        </a>
-        <span class="vr"></span>
-        <footer>
-            <h1><?= htmlspecialchars($lis['price']) ?></h1>
-            <div>
-                <button
-                    type="button"
-                    onclick="event.preventDefault(); event.stopPropagation(); favourite(event)"
-                    class="<?= $lis['is_favourited'] ? 'btn-red' : '' ?>"
-                    data-listing-id="<?= urlencode($lis['listing_id']) ?>"
-                    aria-label="<?= sprintf($lis['is_favourited'] ? "Usuń %s z ulubionych" : "Dodaj %s do ulubionych", htmlspecialchars($lis['title'])) ?>">
-                        <?= $lis['is_favourited'] ? "Usuń z ulubionych" : "Dodaj do ulubionych" ?>
-                </button>
-                <button
-                    type="button"
-                    class="btn-accent"
-                    onclick="event.preventDefault(); event.stopPropagation(); message(event)"
-                    data-listing-id="<?= urlencode($lis['listing_id']) ?>"
-                    aria-label="Skontaktuj się z sprzedającym na temat '<?= htmlspecialchars($lis['title']) ?>'">
-                        Napisz do ogłoszeniodawcy
-                </button>
-            </div>
-        </footer>
-    </article>
-</div>
+        <main>
+            <h2><?= htmlspecialchars($lis['title']) ?></h2>
+            <ul>
+                <li><p><?= htmlspecialchars($lis['display_name']) ?></p></li>
+                <li><p><?= htmlspecialchars($lis['created_at']) ?></p></li>
+            </ul>
+            <span class="small-text">Naciśnij aby wyświetlić więcej szczegółów</span>
+        </main>
+    </a>
+    <span class="vr"></span>
+    <footer>
+        <h1><?= htmlspecialchars($lis['price']) ?></h1>
+        <div>
+            <button
+                type="button"
+                onclick="event.preventDefault(); event.stopPropagation(); favourite(event)"
+                class="<?= $lis['is_favourited'] ? 'btn-red' : '' ?>"
+                data-listing-id="<?= urlencode($lis['listing_id']) ?>"
+                aria-label="<?= sprintf($lis['is_favourited'] ? "Usuń %s z ulubionych" : "Dodaj %s do ulubionych", htmlspecialchars($lis['title'])) ?>">
+                    <?= $lis['is_favourited'] ? "Usuń z ulubionych" : "Dodaj do ulubionych" ?>
+            </button>
+            <button
+                type="button"
+                class="btn-accent"
+                onclick="event.preventDefault(); event.stopPropagation(); message(event)"
+                data-listing-id="<?= urlencode($lis['listing_id']) ?>"
+                aria-label="Skontaktuj się z sprzedającym na temat '<?= htmlspecialchars($lis['title']) ?>'">
+                    Napisz do ogłoszeniodawcy
+            </button>
+        </div>
+    </footer>
+</article>
 <?php endforeach; ?>
 <?php if (!empty($listing->listAll($page + 1, $_SESSION['user_id']))): ?>
 <div id="sentinel" data-next-page="<?= $page + 1 ?>"></div>
