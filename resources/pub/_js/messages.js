ef10bf1de0c5faa8d4791c67ff0ee938d6b4130f
Przemysław Szafraniec <39964594+Nimplex@users.noreply.github.com>
2025-12-01 10:43:24 +0100

    Implement switching chats

--- resources/pub/_js/messages.js
+++ resources/pub/_js/messages.js
@@ -1,3 +1,11 @@
 window.openChat = function (event) {
+  const target = event.target.closest("button");
+  const { chatId } = target.dataset;
+  console.log(target.dataset, chatId);
 
-}
+  const locationUrl = new URL(window.location);
+  locationUrl.search = "";
+  locationUrl.pathname = `/messages/${chatId}`;
+
+  window.location.assign(locationUrl.toString());
+};
